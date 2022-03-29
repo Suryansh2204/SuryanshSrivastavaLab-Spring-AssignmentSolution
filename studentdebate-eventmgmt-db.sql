76ce46320a34b676477fbffa66d7e9a6242257f2
Suryansh2204 <93089882+Suryansh2204@users.noreply.github.com>
2022-03-29 14:54:27 +0530

    db script commit
    sql script to create the database and insert the users and roles

--- studentdebate-eventmgmt-db.sql
+++ studentdebate-eventmgmt-db.sql
@@ -1,8 +1,6 @@
 CREATE DATABASE studentsdebate_eventmgmt;
 use studentsdebate_eventmgmt;
 
-insert into book(author, category, name) values("Lord of the Rings"	,"fantasy Adventure	J.R.R." ,"Tolkien");
-
 insert into roles(name) values("ADMIN");
 insert into roles(name) values("USER");
 
@@ -12,4 +10,4 @@ insert into users(username, password) values("USER2","$2a$12$pvrQdGfu03wXAuym5V8
 insert into user_roles values(1,1);
 insert into user_roles values(2,2);
 
-drop database studentsdebate_eventmgmt;
+#drop database studentsdebate_eventmgmt;
